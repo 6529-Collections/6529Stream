95c4de9d0d2a0960d378c625cf3fdeb5046a92eb
Punk 6529 <108035228+punk6529@users.noreply.github.com>
2026-09-20 17:38:01 +0000

    Use an explicit payment import in current price tests

--- test/current/StreamCurrentAllowlistPrice.t.sol
+++ test/current/StreamCurrentAllowlistPrice.t.sol
@@ -8,7 +8,7 @@ import "../mocks/MockStreamPaymentToken.sol";
 import {
     StreamPrimarySaleSettlement
 } from "../../smart-contracts/domains/revenue/StreamPrimarySaleSettlement.sol";
-import "../../smart-contracts/domains/revenue/StreamERC20PrimarySettlementAdapter.sol";
+import { StreamERC20PrimarySettlementAdapter } from "../../smart-contracts/domains/revenue/StreamERC20PrimarySettlementAdapter.sol";
 import {StreamUniversalAllowlistPriceSale} from "../../smart-contracts/domains/mint/StreamUniversalAllowlistPriceSale.sol";
 import {StreamUniversalAllowlistPrice} from "../../smart-contracts/domains/mint/StreamUniversalAllowlistPrice.sol";
 import {IStreamUniversalAllowlistPriceSale} from "../../smart-contracts/interfaces/stream/mint/IStreamUniversalAllowlistPriceSale.sol";
