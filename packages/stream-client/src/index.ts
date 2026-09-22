f8c8c0a78da82ee3245419313ca9118a81256c55
Punk 6529 <108035228+punk6529@users.noreply.github.com>
2026-09-22 05:43:24 +0000

    Export recovered dispute hydration client and workflow

--- packages/stream-client/src/index.ts
+++ packages/stream-client/src/index.ts
@@ -153,3 +153,5 @@ export * from "./current-artist-recovered-multiple-generation-hydration.js";
 export * from "./current-artist-recovered-multiple-generation-hydration-workflow.js";
 export * from "./current-artist-unbound-platform-hydration.js";
 export * from "./current-artist-unbound-platform-hydration-workflow.js";
+export * from "./current-artist-recovered-multiple-dispute-hydration.js";
+export * from "./current-artist-recovered-multiple-dispute-hydration-workflow.js";
