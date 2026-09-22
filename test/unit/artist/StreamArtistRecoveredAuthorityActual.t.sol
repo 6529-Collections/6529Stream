80e92a2a9fd824349a1f1768c02ca53d0a0d971c
Punk 6529 <108035228+punk6529@users.noreply.github.com>
2026-09-22 04:49:34 +0000

    test: detach Finality construction from recovered hosts
    (cherry picked from commit 0e07124cddc65f1428106ba55bd5c606ae286ab4)

--- test/unit/artist/StreamArtistRecoveredAuthorityActual.t.sol
+++ test/unit/artist/StreamArtistRecoveredAuthorityActual.t.sol
@@ -1169,7 +1169,10 @@ contract StreamArtistRecoveredAuthorityActualTest is StreamArtistGuardianSuperse
     function _rhNext() private returns (Successor memory next) {
         T.SuiteConfiguration memory s = suite;
         address governance = manager.governanceAuthority();
-        ArtistSanctionFinalityFixture finalityFixture = new ArtistSanctionFinalityFixture();
+        ArtistSanctionFinalityFixture finalityFixture = ArtistSanctionFinalityFixture(_artistArtifactCreate(
+            "test/unit/artist/ArtistSanctionFinalityFixture.sol:ArtistSanctionFinalityFixture",
+            abi.encode()
+        ));
         uint256 nonce = avm.getNonce(address(this));
         address registry_ = avm.computeCreateAddress(address(this), nonce);
         address archive_ = avm.computeCreateAddress(address(this), nonce + 1);
